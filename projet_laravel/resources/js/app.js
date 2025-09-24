20931cd0df498fcdd77f705e91312e45ca2cc108
achref <148441738+achref140@users.noreply.github.com>
2025-09-24 21:06:11 +0100

    Integration template

---
+++ projet_laravel/resources/js/app.js
@@ -0,0 +1,10 @@
+import "../vendor/jquery/jquery.min.js";
+import "../vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "../vendor/jquery-easing/jquery.easing.min.js";
+import "../vendor/chart.js/Chart.min.js";
+import "../vendor/fontawesome-free/js/all.min.js";
+import "../vendor/datatables/jquery.dataTables.min.js";
+import "../vendor/datatables/dataTables.bootstrap4.min.js";
+// If you have custom scripts (e.g., sb-admin-2.min.js) place them under resources/vendor and import here
+// import "../assets/js/sb-admin-2.min.js";
+
