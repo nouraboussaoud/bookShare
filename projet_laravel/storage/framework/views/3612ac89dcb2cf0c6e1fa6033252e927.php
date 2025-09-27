008cd75c39a584f318b770030266e592ab8b25db
nouraboussaoud <nour.aboussaoud@esprit.tn>
2025-09-27 19:06:57 +0100

    update gitignore

--- projet_laravel/storage/framework/views/3612ac89dcb2cf0c6e1fa6033252e927.php
+++
@@ -1,8 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
-        </div>
-    </div>
-</footer>
-<?php /**PATH C:\Users\Lenovo\Desktop\bookShare\projet_laravel\resources\views/partials/footer.blade.php ENDPATH**/ ?>
